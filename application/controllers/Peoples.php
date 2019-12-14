f3434f959a0c0d90eae71ccc257c6b7270163e61
Tasrifin <tasrifin.0416@students.amikom.ac.id>
2019-12-14 16:48:41 +0700

    Finishing

--- application/controllers/Peoples.php
+++ application/controllers/Peoples.php
@@ -1,60 +1,59 @@
 <?php
-    class Peoples extends CI_Controller{
+class Peoples extends CI_Controller
+{
 
-        public function index()
-        {
-            $data['judul']='List of Peoples';
-            $this->load->model('Peoples_model', 'peoples');
+    public function index()
+    {
+        $data['judul'] = 'List of Peoples';
+        $this->load->model('Peoples_model', 'peoples');
 
-            //PAGINATION
-            $this->load->library('pagination');
+        //PAGINATION
+        $this->load->library('pagination');
 
-            //CONFIG
-            $config['base_url'] = 'http://192.168.64.3/learn-ci/peoples/index';
-            $config['total_rows'] = $this->peoples->countAllPeoples();
-            $config['per_page'] = 12;
-            $config['num_links']=5;
-            
-            //styling pagination
+        //CONFIG
+        $config['base_url'] = 'http://localhost/ci-crudpagination/peoples/index';
+        $config['total_rows'] = $this->peoples->countAllPeoples();
+        $config['per_page'] = 12;
+        $config['num_links'] = 5;
 
-            $config['full_tag_open']= '<nav aria-label="Page navigation example"> <ul class="pagination justify-content-center">';
-            $config['full_tag_close']= '</ul> </nav>';
+        //styling pagination
 
-            $config['first_link']= 'First';
-            $config['first_tag_open']= '<li class="page-item">';
-            $config['first_tag_close']= '</li>';
+        $config['full_tag_open'] = '<nav aria-label="Page navigation example"> <ul class="pagination justify-content-center">';
+        $config['full_tag_close'] = '</ul> </nav>';
 
-            $config['last_link']= 'Last';
-            $config['last_tag_open']= '<li class="page-item">';
-            $config['last_tag_close']= '</li>';
+        $config['first_link'] = 'First';
+        $config['first_tag_open'] = '<li class="page-item">';
+        $config['first_tag_close'] = '</li>';
 
-            $config['next_link']= '&raquo';
-            $config['next_tag_open']= '<li class="page-item">';
-            $config['next_tag_close']= '</li>';
+        $config['last_link'] = 'Last';
+        $config['last_tag_open'] = '<li class="page-item">';
+        $config['last_tag_close'] = '</li>';
 
-            $config['prev_link']= '&laquo';
-            $config['prev_tag_open']= '<li class="page-item">';
-            $config['prev_tag_close']= '</li>';
-            
-            $config['cur_tag_open']= '<li class="page-item active"><a class="page-link" href="#">';
-            $config['cur_tag_close']= '</a></li>';
-            
-            $config['num_tag_open']= '<li class="page-item">';
-            $config['num_tag_close']= '</li>';
+        $config['next_link'] = '&raquo';
+        $config['next_tag_open'] = '<li class="page-item">';
+        $config['next_tag_close'] = '</li>';
 
-            $config['attributes'] = array('class' => 'page-link');
+        $config['prev_link'] = '&laquo';
+        $config['prev_tag_open'] = '<li class="page-item">';
+        $config['prev_tag_close'] = '</li>';
 
-            //INISIALISASI
-            $this->pagination->initialize($config);
+        $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
+        $config['cur_tag_close'] = '</a></li>';
 
-            
-            $data['start']=$this->uri->segment(3);
-            $data['peoples']=$this->peoples->getPeoples($config['per_page'], $data['start']);
+        $config['num_tag_open'] = '<li class="page-item">';
+        $config['num_tag_close'] = '</li>';
 
-            $this->load->view('templates/header',$data);
-            $this->load->view('peoples/index',$data);
-            $this->load->view('templates/footer');
-        }
+        $config['attributes'] = array('class' => 'page-link');
 
-        
+        //INISIALISASI
+        $this->pagination->initialize($config);
+
+
+        $data['start'] = $this->uri->segment(3);
+        $data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start']);
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('peoples/index', $data);
+        $this->load->view('templates/footer');
     }
+}
